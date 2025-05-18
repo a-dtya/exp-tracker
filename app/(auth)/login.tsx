d4137f28fcc83f7b269d20f56928cae814fc2cfd
a-dtya <adithyavinod1943@gmail.com>
2025-05-18 18:21:06 +0530

    form created in login screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -3,12 +3,46 @@ import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import { spacingY, spacingX } from '@/constants/theme'
 import BackButton from '@/components/BackButton'
+import { TextInput } from 'react-native'
+import { useRef } from 'react'
+import Button from '@/components/Button'
+import { verticalScale } from '@/utils/styling'
 
 const Login = () => {
+  const emailRef = useRef<string>("");
+  const passwordRef = useRef<string>("");
+  const handleSubmit = async() => {
+  }
+
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <BackButton/>
+        <View>
+          <Text>Hey there,</Text>
+          <Text>Welcome back!</Text>
+        </View>
+        <View>
+          <Text>Login to your account</Text>
+          <TextInput
+            onChangeText={(text) => emailRef.current = text}
+            placeholder="Email"
+            style={styles.input}
+            value={emailRef.current}
+          />
+          <TextInput
+            onChangeText={(text) => passwordRef.current = text}
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry
+            value={passwordRef.current}
+          />
+        </View>
+        <Button
+          onPress={handleSubmit}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </Button>
       </View>
     </ScreenWrapper>
   )
@@ -23,4 +57,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'rgba(230, 193, 81, 0.83)',
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 12,
+    width: '100%',
+  },
+  buttonText: {
+    fontSize: verticalScale(16),
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  }
 })
