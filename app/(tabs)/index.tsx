e8d20b4af6e1ea8d7f2554f13b72a0c39614a5ec
a-dtya <adithyavinod1943@gmail.com>
2025-05-20 00:24:03 +0530

    added signout fn in (tabs) index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from '@/components/Button'
+import { signOut } from 'firebase/auth'
+import { auth } from '@/config/firebase'
+import { verticalScale } from '@/utils/styling'
 
 const Home = () => {
+  const handleLogOut = async() => {
+    await signOut(auth)
+  }
+    
   return (
     <View>
       <Text>Home</Text>
+      <Button onPress={handleLogOut}>
+        <Text style={styles.text}>Logout</Text>
+      </Button>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    fontSize: verticalScale(16),
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+})
